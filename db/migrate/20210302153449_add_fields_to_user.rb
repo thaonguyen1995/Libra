5d650be990cd14b2074fce4b48fd4b879b8346b8
Nguyen Van Thao <thaonguyen@nguyen.van.thao>
2021-03-02 23:16:17 +0700

    [Feature] Login

---
+++ db/migrate/20210302153449_add_fields_to_user.rb
@@ -0,0 +1,10 @@
+class AddFieldsToUser < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :slug, :string
+    add_index :users, :slug, unique: true
+    add_column :users, :user_name, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+  end
+end
