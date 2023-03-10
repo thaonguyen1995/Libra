841ebc8a86115d810efcb62f12d00635b1261035
thaonv-2301 <nguyen.van.thao@sun-asterisk.com>
2023-03-10 14:41:53 +0700

    change database to mysql

--- Capfile
+++ Capfile
@@ -13,6 +13,7 @@ require 'capistrano/rbenv'
 require 'capistrano/rails/assets' # for asset handling add
 require 'capistrano/rails/migrations' # for running migrations
 require 'capistrano/puma'
+require 'capistrano/figaro_yml'
 install_plugin Capistrano::Puma
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
--- config/puma.rb
+++ config/puma.rb
@@ -2,11 +2,11 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 port        ENV.fetch("PORT") { 3000 }
 app_dir = File.expand_path("../..", __FILE__)
-environment "production"
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-bind "unix:///home/deploy/libra/shared/tmp/sockets/puma.sock"
-pidfile '/home/deploy/libra/shared/tmp/pids/puma.pid'
-state_path '/home/deploy/libra/shared/tmp/pids/puma.state'
+# bind "unix:///home/deploy/libra/shared/tmp/sockets/puma.sock"
+# pidfile '/home/deploy/libra/shared/tmp/pids/puma.pid'
+# state_path '/home/deploy/libra/shared/tmp/pids/puma.state'
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 plugin :tmp_restart
