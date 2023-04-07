3c789b32127347d788cb1f37e59d538d4a0a13a0
thaonv-2301 <nguyen.van.thao@sun-asterisk.com>
2023-04-07 16:15:34 +0700

    set rails_env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,6 +8,6 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :branch, 'react_rails7'
 set :stage, :production
-set :rails_env, :production
+set :rails_env, 'production'
 set :deploy_to, "/home/deploy/libra"
 server "54.152.117.159", user: "deploy", roles: %w(web db app)
